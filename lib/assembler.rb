ef567333a9ff65cec6b26449a54d0de775ddb367
Mark Skelton <markskelton10@gmail.com>
2025-07-25 13:53:44 +0100

    feat!: add pre-defined symbols to @variables
    Such as R0 & KBD.
    
    BREAKING CHANGE: Use of pre-defined symbols now maps to corresponding
    integer instead of giving an error

--- lib/assembler.rb
+++ lib/assembler.rb
@@ -9,7 +9,7 @@ require_relative 'code'
 # It then translates each symbol into binary code, with the help of Code
 # Finally, it appends it all together and puts it into the output file
 class Assembler
-  attr_reader :parser, :code, :output_file
+  attr_reader :parser, :code, :output_file, :symbol_table
   attr_accessor :counter
 
   def initialize
@@ -17,6 +17,29 @@ class Assembler
     @code = Code.new
     @counter = 0
     @output_file = File.open("#{ARGV[0].split('.')[0]}.hack", 'w')
+    @symbol_table = { 'R0' => '0',
+                      'R1' => '1',
+                      'R2' => '2',
+                      'R3' => '3',
+                      'R4' => '4',
+                      'R5' => '5',
+                      'R6' => '6',
+                      'R7' => '7',
+                      'R8' => '8',
+                      'R9' => '9',
+                      'R10' => '10',
+                      'R11' => '11',
+                      'R12' => '12',
+                      'R13' => '13',
+                      'R14' => '14',
+                      'R15' => '15',
+                      'SP' => '0',
+                      'LCL' => '1',
+                      'ARG' => '2',
+                      'THIS' => '3',
+                      'THAT' => '4',
+                      'SCREEN' => '16384',
+                      'KBD' => '24576' }
   end
 
   def asm_to_binary
@@ -42,8 +65,10 @@ class Assembler
   end
 
   def to_16bit
+    symbol = parser.symbol
+    number = symbol.match?(/^\d+$/) ? symbol : symbol_table[symbol]
     # Note that all A-instructions/addresses must begin with a 0
-    "0#{format('%015b', parser.symbol)[-15..]}"
+    "0#{format('%015b', number)[-15..]}"
   rescue StandardError
     close_files
     raise "Error: Invalid instruction #{parser.instruction} at line #{counter}"
